cd9d039e51bc3790afb1babc24bea6b95bb195e0
jamiepollock1 <122102690+jamiepollock1@users.noreply.github.com>
2023-01-27 17:42:05 +0000

    Update script.js
    playRound now runs in console okay, next task is to add game function to loop rounds and record scores

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // First I assign the fixed options. The [] is used
 // because the options are strings
 
-const options = (["Rock","Paper","Scissors"]);
-const playerTurn = ("Rock")
+const options = (["rock","paper","scissors"]);
 
 // This function below will have the computer generate
 // a random choice for use in each round
@@ -17,8 +16,20 @@ function getComputerChoice(options) {
 // Now carry the result of getComputerChoice over and
 // assign it to compTurn for use in the game
 
-let compTurn = getComputerChoice(options);
-console.log(compTurn);
-
 // Now this next function will play a round of the game
 // using the playerTurn and compTurn parameters
+
+function playRound(playerTurn, computerSelection) {
+
+    if (playerTurn === computerSelection) {
+        return "It's a tie!";
+    } else if (playerTurn === "rock" && computerSelection === "scissors" || playerTurn === "scissors" && computerSelection === "paper" || playerTurn === "paper" && computerSelection === "rock") {
+        return "You win!";
+    } else {
+        return "You lose!";
+    }
+}
+
+let computerSelection = getComputerChoice(options);
+let playerTurn = "rock";
+console.log(playRound(playerSelection, compTurn));
