3cf23921aedc8df6b91a19d59a150b23ed9bff14
jamiepollock1 <122102690+jamiepollock1@users.noreply.github.com>
2023-01-27 17:09:22 +0000

    Update script.js
    Script now carries computer choice over ready for later use

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+// First I assign the fixed options. The [] is used
+// because the options are strings
+
 const options = (["Rock","Paper","Scissors"]);
+const playerTurn = ("Rock")
+
+// This function below will have the computer generate
+// a random choice for use in each round
 
 function getComputerChoice(options) {
     let randomIndex = Math.floor(Math.random() * options.length);
@@ -6,3 +13,12 @@ function getComputerChoice(options) {
     console.log(computerSelection);
     return computerSelection;
 }
+
+// Now carry the result of getComputerChoice over and
+// assign it to compTurn for use in the game
+
+let compTurn = getComputerChoice(options);
+console.log(compTurn);
+
+// Now this next function will play a round of the game
+// using the playerTurn and compTurn parameters
